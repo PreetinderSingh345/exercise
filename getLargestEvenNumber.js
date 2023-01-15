6163fb6017e3402f6557fc00ff6f818f70a72fc0
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 18:57:42 +0530

    feat(math): implement largest even number in array functionality

---
+++ getLargestEvenNumber.js
@@ -0,0 +1,21 @@
+const isEven =(num)=> (num%2==0);
+
+function getBiggestEvenNumInArr(arr) {
+    let maxEvenValue=-1;
+
+    arr.forEach(value => {
+        maxEvenValue=((maxEvenValue==-1)? (isEven(value)? value: -1): (isEven(value)? ((value>maxEvenValue)? value: maxEvenValue): maxEvenValue));
+    });
+
+    return maxEvenValue;
+}
+
+const getBiggestEvenNumInArrArrow=(arr)=> {
+    let maxEvenValue=-1;
+
+    arr.forEach(value => {
+        maxEvenValue=((maxEvenValue==-1)? (isEven(value)? value: -1): (isEven(value)? ((value>maxEvenValue)? value: maxEvenValue): maxEvenValue));
+    });
+
+    return maxEvenValue;
+}
