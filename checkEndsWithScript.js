82da55361129072af01fd42a0564f5186f9a887a
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 22:49:56 +0530

    chore: add tests to check if script ends with 'Script'

--- checkEndsWithScript.js
+++ checkEndsWithScript.js
@@ -2,7 +2,12 @@ function endsWithScript(str) {
     return str.endsWith("Script");
 }
 
-const endsWithScriptArrow =()=> str.endsWith('Script');
+const endsWithScriptArrow =(str)=> str.endsWith('Script');
 
 console.log(endsWithScript('Script'));
 console.log(endsWithScript('doesnotendwithcapitalscript'))
+
+module.exports = {
+    endsWithScript,
+    endsWithScriptArrow
+}
---
+++ checkEndsWithScript.test.js
@@ -0,0 +1,17 @@
+const {endsWithScript, endsWithScriptArrow} = require('./checkEndsWithScript')
+
+test('simple function', ()=> {
+    expect(endsWithScript("Script")).toBe(true)
+})
+
+test('simple function', ()=> {
+    expect(endsWithScript('script')).toBe(false)
+})
+
+test('arrow function', ()=> {
+    expect(endsWithScriptArrow('doesEndWithScript')).toBe(true)
+})
+
+test('arrow function', ()=> {
+    expect(endsWithScriptArrow('doesNotEndWithscript')).toBe(false)
+})
