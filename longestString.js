418e7aab3182f7d222dc00c565cbe661aefbf78a
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 20:04:09 +0530

    feat(string): add functionality to find the longest string in an array

---
+++ longestString.js
@@ -0,0 +1,19 @@
+function findLongestString(arr) {
+    let longestStr="";
+
+    arr.forEach(str => {
+        longestStr=((str.length>longestStr.length)? str: longestStr);
+    });
+
+    return longestStr;
+}
+
+const findLongestStringArrow =(arr)=> {
+    let longestStr="";
+
+    arr.forEach(str => {
+        longestStr=((str.length>longestStr.length)? str: longestStr);
+    });
+
+    return longestStr;
+}
