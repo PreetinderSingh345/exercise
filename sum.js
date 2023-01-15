6f69583b208355f84de32ed88388f39c2691fdb4
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 16:33:24 +0530

    chore: write armstrong and prime function file

--- sum.js
+++ sum.js
@@ -2,4 +2,8 @@ function sum(a, b) {
     return a+b
 }
 
+function diff(a, b) {
+    return a-b;
+}
+
 module.exports=sum;
--- sum.test.js
+++ sum.test.js
@@ -11,3 +11,5 @@ test('adds 2+3 to equal 5', ()=> {
 test ('adds 5+7 to equal 12', ()=> {
     expect(sum(5, 7)).toBe(12);
 })
+
+test
