bc738ed83045d12b46fe4414c4a127994a7cd2c2
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 19:18:04 +0530

    feat(math): add functionality to check if number has all digits same

---
+++ checkAllDigitsSame.js
@@ -0,0 +1,45 @@
+function areAllDigitsSame(num) {
+    let copy=num;
+
+    let areSame=true;
+    let firstDigit=-1;
+
+    while(copy!=0) {
+        let digit=(copy%10);
+
+        firstDigit=((firstDigit==-1)? digit: firstDigit);
+
+        if(digit!=firstDigit) {
+            areSame=false;
+
+            break;
+        }
+
+        copy=Math.floor(copy/10);
+    }
+
+    return areSame;
+}
+
+ const areAllDigitsSameArrow =(num)=> {
+    let copy=num;
+
+    let areSame=true;
+    let firstDigit=-1;
+
+    while(copy!=0) {
+        let digit=(copy%10);
+
+        firstDigit=((firstDigit==-1)? digit: firstDigit);
+
+        if(digit!=firstDigit) {
+            areSame=false;
+
+            break;
+        }
+
+        copy=Math.floor(copy/10);
+    }
+
+    return areSame;
+ }
