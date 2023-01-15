9abd22316b084e665a8785adcfe73e11e519c834
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 19:53:36 +0530

    feat(string): add functionality to reverse case of string

---
+++ changeCapitalization.js
@@ -0,0 +1,28 @@
+const letterPattern=/[a-zA-Z]/;
+
+const isLetter =(char)=> char.match(letterPattern)!=null;
+
+function changeCaseOfString(str) {
+    let changedStr="";
+
+    for(let char of str) {
+
+        let charToAdd=isLetter(char)? ((char==char.toUpperCase())? char.toLowerCase(): char.toUpperCase()): char;
+
+        changedStr+=charToAdd;
+    }
+
+    return changedStr;
+}
+
+const changeCaseOfStringArrow =(str)=> {
+    let changedStr="";
+
+    [...str].forEach(char => {
+        let charToAdd=isLetter(char)? ((char==char.toUpperCase())? char.toLowerCase(): char.toUpperCase()): char;
+
+        changedStr+=charToAdd;
+    });
+
+    return changedStr;
+}
