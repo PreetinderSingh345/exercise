1c2aadea5fae57ee324ae07c79ee92e066cabd67
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 23:59:13 +0530

    feat(string): add functionality to convert each letter in string to its lexical successor

---
+++ makeEachLetterEqualToItsSuccessor/makeEachLetterEqualToItsSuccessor.js
@@ -0,0 +1,26 @@
+function makeEachLetterInStringEqualToItsLexicalSuccessor(str) {
+    let convertedString="";
+
+    [...str].forEach((char)=> {
+        let lexicallyNextChar=String.fromCharCode(((char.charCodeAt()-96)%26)+97)
+
+        convertedString+=lexicallyNextChar
+    })
+
+    return convertedString
+}
+
+const makeEachLetterInStringEqualToItsLexicalSuccessorArrow =(str)=> {
+    let convertedString="";
+
+    [...str].forEach((char)=> {
+        let lexicallyNextChar=String.fromCharCode(((char.charCodeAt()-96)%26)+97)
+
+        convertedString+=lexicallyNextChar
+    })
+
+    return convertedString
+}
+
+console.log(makeEachLetterInStringEqualToItsLexicalSuccessor('codeacademy'))
+console.log(makeEachLetterInStringEqualToItsLexicalSuccessorArrow('lazyinterns'))
