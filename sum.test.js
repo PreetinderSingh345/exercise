e2e88435e4b8b8bf65cdf15c08a7fcbef3680cf1
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 16:09:33 +0530

    chore: add more tests

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum=require('./sum');
 test('adds 1+2 to equal 3', ()=> {
     expect(sum(1, 2)).toBe(3);
 })
+
+test('adds 2+3 to equal 5', ()=> {
+    expect(sum(2, 3)).toBe(5);
+})
+
+test ('adds 5+7 to equal 12', ()=> {
+    expect(sum(5, 7)).toBe(12);
+})
