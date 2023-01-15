1771f608b8ba30435bf805e859acc1f049c9f8c6
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-01-15 23:00:50 +0530

    feat(string): add functionality to concatenate two strings after removing their first letters

---
+++ concatWithoutFirstLetter.js
@@ -0,0 +1,18 @@
+function concatenateWithoutFirstLetter(arr) {
+    let firstChar, secondChar;
+
+    [firstChar, secondChar]=arr;
+
+    return (firstChar.slice(1).concat(secondChar.slice(1)));
+}
+
+const concatenateWithoutFirstLetterArrow =(arr)=> {
+    let firstChar, secondChar;
+
+    [firstChar, secondChar]=arr;
+
+    return (firstChar.slice(1).concat(secondChar.slice(1)));
+}
+
+console.log(concatenateWithoutFirstLetter(['code', 'academy']))
+console.log(concatenateWithoutFirstLetterArrow(['arrow', 'function']))
